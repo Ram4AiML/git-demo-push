ffeab47a3f5dd404bf9b50645330717fe8c6cc23
ram <ram.ai@outlook.com>
2024-01-19 20:22:19 +0530

    Add doc string to div()

--- div.py
+++ div.py
@@ -1,4 +1,14 @@
 def div(a,b):
+    """
+    Division two numbers.
+
+    Parameters:
+    - a (int): The first number.
+    - b (int): The second number.
+
+    Returns:
+    int: The Division of a and b.
+    """
     return a / b
 
 print("Division of a and b :", div(2,3))
