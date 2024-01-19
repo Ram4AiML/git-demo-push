6f1bec5dbaf0d4077360b7c99ac78cfba4530190
ram <ram.ai@outlook.com>
2024-01-19 12:40:06 +0530

    Add doc string : Enhnaced the code readability by adding doc string

--- mul.py
+++ mul.py
@@ -1,4 +1,14 @@
 def mul(a,b):
+    """
+    Multiply two numbers.
+
+    Parameters:
+    - a (int): The first number.
+    - b (int): The second number.
+
+    Returns:
+    int: The product of a and b.
+    """
     return a * b
 
 print("multiplication of a and b :", mul(2,3))
