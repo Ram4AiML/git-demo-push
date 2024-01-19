97ddbce87fe6cbe87ceb18423fd2aa58c1da2842
ram <ram.ai@outlook.com>
2024-01-19 12:36:41 +0530

    Add doc string : Enhanced the code readability by adding doc string

--- sum.py
+++ sum.py
@@ -1,4 +1,14 @@
 def sum(a,b):
+    """
+    Add two numbers.
+
+    Parameters:
+    - a (int): The first number.
+    - b (int): The second number.
+
+    Returns:
+    int: The sum of a and b.
+    """
     return a + b
 
 print("Sum of a and b :", sum(2,3))
